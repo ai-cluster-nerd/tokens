1ddabbf9cc4826e9a4d755d9eee3b1b7b7d37543
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-25 15:38:40 +0100

    Update src/main.py

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import datetime
+import logging
+import os
+import sys
+
+import boto3
+
+
+def main():
+    """
+    Entry po
+
+    :return:
+    """
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+
+if __name__ == '__main__':
+
+    # Paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Classes
+    import src.elements.s3_parameters as s3p
+    import src.elements.service as sr
+    import src.functions.cache
+    import src.preface.interface
+
+    connector: boto3.session.Session
+    s3_parameters: s3p
+    service: sr.Service
+    connector, s3_parameters, service = src.preface.interface.Interface().exc()
+
+    main()
